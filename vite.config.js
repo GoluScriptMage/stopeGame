d13a78a5e24cbe5af73f9c61104e1d107c1fb2a2
Golu Dhakad <goluji9659@gmail.com>
2024-06-29 22:19:58 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stopeGame', // Replace with your repo name
+  base: '/stopeGame/', // Replace with your repo name
 });
